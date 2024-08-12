25ea5d1bf4387c1be75047bb5e57f2ba482777a7
Advik Sharma <70201060+advik-student-dev@users.noreply.github.com>
2024-08-12 16:11:31 -0700

    choreo added

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,23 +1,24 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+const apiUrl = "/choreo-apis/django-react-tutorial/backend/v1";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiUrl,
 });
 
 api.interceptors.request.use(
   (config) => {
-    console.log("base_url: " + import.meta.env.VITE_API_URL)
+    console.log("base_url: " + import.meta.env.VITE_API_URL);
     const token = localStorage.getItem(ACCESS_TOKEN);
-    console.log('Retrieved token in interceptor:', token);
+    console.log("Retrieved token in interceptor:", token);
     if (token) {
       config.headers.Authorization = `Bearer ${token}`;
     }
     return config;
   },
   (error) => {
-    console.error('Request interceptor error:', error);
+    console.error("Request interceptor error:", error);
     return Promise.reject(error);
   }
 );
